9e8ac057668b92a352e6ea1fe07ddc9915016b79
takashitajima <t.takashi@ceres.ocn.ne.jp>
2018-01-27 00:23:08 +0900

    新規 myEverythingComponent

--- mdapi/aura/harnessApp/harnessAppController.js
+++ mdapi/aura/harnessApp/harnessAppController.js
@@ -1,5 +1,7 @@
 ({
-    myAction : function(component, event, helper) {
-
+    handleClick: function(component, event, helper) {
+        var btnClicked = event.getSource();
+        var btnMessage = btnClicked.get("v.label");
+        component.set("v.message", btnMessage);
     }
 })
