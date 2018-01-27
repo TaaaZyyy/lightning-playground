df18173d59f196fdf5d92a5844fd38a9c36ebc3d
takashitajima <t.takashi@ceres.ocn.ne.jp>
2018-01-27 15:09:25 +0900

    modify

--- force-app/main/default/aura/AccountList/AccountListHelper.js
+++ force-app/main/default/aura/AccountList/AccountListHelper.js
@@ -4,6 +4,7 @@
         var action = component.get('c.getAccounts');
 
         // コールバック
+        var self = this;
         action.setCallback(this, function(actionResult) {
             component.set('v.accounts', actionResult.getReturnValue());
         });
