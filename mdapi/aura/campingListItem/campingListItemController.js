a55d3c8f72cea317a04c41af97d278fad2fe739b
takashitajima <t.takashi@ceres.ocn.ne.jp>
2018-01-28 22:46:34 +0900

    add campingListItemButton

--- mdapi/aura/campingListItem/campingListItemController.js
+++ mdapi/aura/campingListItem/campingListItemController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    packItem : function(component, event, helper) {
+        var item = component.get("v.item");
+        item.Packed__c = true;
+        component.set("v.item", item);
+        component.set("v.disabled", true);
+    },
 })
