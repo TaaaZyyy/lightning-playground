17df68b0bad57a8eceb96583334636908fe9adf6
takashitajima <t.takashi@ceres.ocn.ne.jp>
2018-02-07 22:40:15 +0900

    lessen Input DataUsing Forms

--- force-app/main/default/aura/Expenses/ExpensesController.js
+++ force-app/main/default/aura/Expenses/ExpensesController.js
@@ -1,22 +1,23 @@
 ({
-    clickCreate : function(component, event, helper) {
-        var validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
-            // findは指定のIDのformの情報にアクセスする
-            // validSoFarはformの情報に一つでも無効な値があるとfalseを返し、そうでない場合trueを返す
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            // inputCmp.showHelpMessageIfInvalid() は、無効な項目のエラーメッセージを表示します
-            return validSoFar && inputCmp.get('v.validity').valid;
-            // inputCmp.get('v.validity').valid は、配列内の現在の入力項目の有効性を返します
-            // <lightning:input> では、デフォルトのエラーメッセージが提供されます
-            // これは、経費フォームの例のように messageWhenRangeUnderflow などの属性を使用してカスタマイズできます。
+    clickCreate: function(component, event, helper) {
+        // 配列.reduce(コールバック関数, 初期値)
+        // 初期値と最初の配列の要素をコールバック関数にかける。その戻り値と次の配列の要素をコールバック関数にかける。繰り返す。
+        // validSoFarは初期値または前回の戻り値。inputElementは配列の要素。
+        // よって、validExpenseはすべてのlightning:inputに有効な値が入っていたらtrueを、一つでも無効な値が入っていたらfalseになる。
+        // また、ここでcomponent.get("v.newExpense")を使わないわけは、messageWhenRangeUnderflow要素でカスタムのエラーメッセージを表示させるため。
+        let validExpense = component.find('expenseform').reduce(function(validSoFar, inputElement) {
+            // 無効な値のエラーメッセージを入力項目の下に表示する
+            // 入力メッセージはデフォルトもしくは属性でカスタマイズできる。↓のmessageWhen~の属性。
+            // https://developer.salesforce.com/docs/atlas.ja-jp.lightning.meta/lightning/aura_compref_lightning_input.htm
+            inputElement.showHelpMessageIfInvalid();
+            // inputElement.get('v.validity').valid で値が有効ならtrue無効ならfalseを表す
+            return validSoFar && inputElement.get('v.validity').valid;
         }, true);
-        // If we pass error checking, do some real work
         if (validExpense) {
-            //Create the new expense
-            var newExpense = component.get("v.newExpense");
+            // 無効な入力値がなかった場合
+            let newExpense = component.get("v.newExpense");
             console.log("Create expense: " + JSON.stringify(newExpense));
             helper.createExpense(component, newExpense);
         }
-    }
+    },
 })
--- force-app/main/default/aura/Expenses/ExpensesHelper.js
+++ force-app/main/default/aura/Expenses/ExpensesHelper.js
@@ -1,12 +1,15 @@
 ({
-    createExpenses: function(component, expense) {
-        var theExpenses = component.get("v.expenses");
+    createExpense: function(component, expense) {
+        let theExpenses = component.get("v.expenses");
 
         // Copy the expense to a new object
-        // THIS IS A DISGUSTING, TEMPORARY HACK
-        var newExpense = JSON.parse(JSON.stringify(expense));
+        // THIS IS A DISGUSTING, THEMPORARY HACK
+        let newExpenses = JSON.parse(JSON.stringify(expense));
 
-        theExpenses.push(newExpense);
+        console.log("Expenses before 'create': " + JSON.stringify(theExpenses));
+        theExpenses.push(newExpenses);
+        // ここではv.expensesの値を空更新することで、v.expensesを使用したすべての場所に更新をカスケードする。
         component.set("v.expenses", theExpenses);
-    }
+        console.log("Expenses after 'create': " + JSON.stringify(theExpenses));
+    },
 })
